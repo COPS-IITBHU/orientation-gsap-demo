c33345075a6e5d9d5d35639d3c7f5b9ea7e2b400
Lakshya Singh <lakshay.singh1108@gmail.com>
2020-12-12 03:07:22 +0530

    header particle done

---
+++ src/components/Header.js
@@ -0,0 +1,58 @@
+import logo from '../images/logo.svg';
+import Particles from 'react-particles-js'
+
+
+export default function Header() {
+  return (
+    <div>
+      <div className="scale-in-center" id="welcome">
+        <img src={logo} alt="COPS" />
+        <span>DEV</span>
+      </div>
+      <Particles id="particles" params={{
+        "particles": {
+          "number": {
+            "value": 160,
+            "density": {
+              "enable": false,
+            }
+          },
+          "color": {
+            "value": "#fff"
+          },
+          "opacity": {
+            "value": 0.5,
+            "anim": {
+              "enable": true
+            }
+          },
+          "size": {
+            "value": 10,
+            "random": true
+          },
+          "move": {
+            "direction": "bottom",
+            "out_mode": "out",
+            "speed": 0.4
+          },
+          "line_linked": {
+            "enable": false
+          }
+        },
+        "interactivity": {
+          "events": {
+            "onclick": {
+              "enable": true,
+              "mode": "remove"
+            }
+          },
+          "modes": {
+            "remove": {
+              "particles_nb": 10
+            }
+          }
+        }
+      }} />
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import Header from './components/Header'
 
 ReactDOM.render(
 	<React.StrictMode>
+		<Header />
 		<App />
 	</React.StrictMode>,
 	document.getElementById('root')
